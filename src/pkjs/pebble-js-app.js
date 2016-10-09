5aceb220681a1c3d3b3e34eff626ab56a7831863
Gavriel Fleischer <flocsy@gmail.com>
2016-10-10 00:28:23 +0300

    make it compile with SDK4
    bug in Aplite: SunGraph, sunrize, sunset, sun-time are incorrect

--- src/pkjs/pebble-js-app.js
+++ src/pkjs/pebble-js-app.js
@@ -1,14 +1,21 @@
 function fetchUtcOffset(latitude, longitude) {
     console.log("FETCHING UTC OFFSET");
+    if (true) { // hack to set coordinates in emulator
+      latitude = 31.828200;
+      longitude = 34.683248;
+      console.log("overridden latitude = " + latitude);
+      console.log("overridden longitude = " + longitude);
+    }
     
-    var bigLat = Math.round(parseFloat(latitude)*1000);             console.log("latitude = " + bigLat);
-    var bigLon = Math.round(parseFloat(longitude)*1000);            console.log("longitude = " + bigLon);
-    
-    var delta_minutes = 0;
+    var bigLat = Math.round(parseFloat(latitude)*1000);
+    console.log("latitude = " + bigLat);
+    var bigLon = Math.round(parseFloat(longitude)*1000);
+    console.log("longitude = " + bigLon);
     
     var today = new Date();
 
-    var delta_minutes = -today.getTimezoneOffset();                  console.log("Timezone Offset = " + delta_minutes);
+    var delta_minutes = -today.getTimezoneOffset();
+    console.log("Timezone Offset = " + delta_minutes);
     
     Pebble.sendAppMessage({
                           "lat":bigLat,
